6b836a0e8d6928d632179b63ecc882affa0c27cf
Amandeep Singh <adsoapps@gmail.com>
2020-07-14 20:15:38 +0530

    Index Scene Completed
    audio, effects completed

---
+++ 3d-auto-expo/Assets/Src/Scripts/SceneLoading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoading : MonoBehaviour
+{
+    public void LoadMySceneInteger(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadMySceneString(string sceneString)
+    {
+        SceneManager.LoadScene(sceneString);
+    }
+
+    public void ReloadMyScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
