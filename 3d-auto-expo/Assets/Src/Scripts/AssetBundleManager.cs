e78a2cd5f903f076be7ca55ae5ad0672743c5c4a
Amandeep Singh <adsoapps@gmail.com>
2020-07-14 18:13:12 +0530

    Asset Bundles
    created, models imported

---
+++ 3d-auto-expo/Assets/Src/Scripts/AssetBundleManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AssetBundleManager : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles() {
+        string abDir = "Assets/AssetBundles";
+
+        if (!Directory.Exists(Application.streamingAssetsPath)) {
+            Directory.CreateDirectory(abDir);
+        }
+
+        BuildPipeline.BuildAssetBundles(abDir, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
