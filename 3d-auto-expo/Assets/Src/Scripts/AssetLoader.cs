04ed57fca4fbaadde3a639afb0cfa5784fe96af4
Amandeep Singh <adsoapps@gmail.com>
2020-07-15 11:13:10 +0530

    Features Added

---
+++ 3d-auto-expo/Assets/Src/Scripts/AssetLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class AssetLoader : MonoBehaviour
+{
+
+    public string bundleURL = "https://drive.google.com/drive/folders/1CnOx1IY8EtwzoA2ODr4VvhwqMC1SzRw7?usp=sharing";
+    public string assetName = "ab-car1";
+
+    IEnumerator Start()
+    {
+        using (WWW web = new WWW(bundleURL)) {
+            yield return web;
+
+            AssetBundle remoteAssetBundle = web.assetBundle;
+            if (remoteAssetBundle == null) {
+                Debug.LogError("Adso failed to download Asset Bundle");
+            }
+            Instantiate(remoteAssetBundle.LoadAsset(assetName));
+            //remoteAssetBundle.Unload(false);
+        }
+        
+    }
+}
---
+++ 3d-auto-expo/Assets/Src/Scripts/PlusMinusMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlusMinusMenu : MonoBehaviour
+{
+    private bool boolFeatureMenu, boolFooterMenu = false;
+    public Animator FeatureMenu, FooterMenu;
+    public Button BtnFeatureMenu, BtnFooterMenu;
+    public Sprite add, sub;
+
+    public void AnimFeatureMenu()
+    {
+        if (!boolFeatureMenu)
+        {
+            boolFeatureMenu = true;
+            FeatureMenu.SetBool("boolCloseOpen", boolFeatureMenu);
+            BtnFeatureMenu.GetComponent<Image>().sprite = add;
+        }
+        else
+        {
+            boolFeatureMenu = false;
+            FeatureMenu.SetBool("boolCloseOpen", boolFeatureMenu);
+            BtnFeatureMenu.GetComponent<Image>().sprite = sub;
+        }
+    }
+    /*
+    public void AnimFooterMenu()
+    {
+        if (!boolFooterMenu)
+        {
+            boolFooterMenu = true;
+            FooterMenu.SetBool("BoolFooterMenu", boolFooterMenu);
+            BtnFooterMenu.GetComponent<Image>().sprite = add;
+        }
+        else
+        {
+            boolFooterMenu = false;
+            FooterMenu.SetBool("BoolFooterMenu", boolFooterMenu);
+            BtnFooterMenu.GetComponent<Image>().sprite = sub;
+        }
+    }
+    */
+}
