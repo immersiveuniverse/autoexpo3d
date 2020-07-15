13ed84e442207ba144e5bf1bce06764cb10949b8
Amandeep Singh <adsoapps@gmail.com>
2020-07-15 20:17:26 +0530

    Ar Mode partially completed

---
+++ 3d-auto-expo/Assets/Src/Scripts/HelpManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpManager : MonoBehaviour
+{
+    public bool boolHelp = false;
+    public Button btnClose, btnHelp;
+    public GameObject HelpPanel;
+
+    public void PanelHelping() {
+        if (!boolHelp)
+        {
+            HelpPanel.SetActive(true);
+            boolHelp = true;
+        }
+        else {
+            HelpPanel.SetActive(false);
+            boolHelp = false;
+        }
+    }
+
+    public void HelpClose() {
+        HelpPanel.SetActive(false);
+        boolHelp = false;
+    }
+}
