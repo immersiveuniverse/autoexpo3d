8b2cdd1a1f3458b9d44a0515b3f1c11e408a0e97
Amandeep Singh <adsoapps@gmail.com>
2020-07-16 06:40:59 +0530

    Color Variant Feature

---
+++ 3d-auto-expo/Assets/Src/Scripts/ColorVariant.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorVariant : MonoBehaviour
+{
+    public GameObject[] cars;
+    public Button[] colors;
+    public Material m1, m2, m3;
+
+    private int i;
+    public void SelectColor(int index) {
+        if (index == 0)
+        {
+            i = 0;
+            FirstColor();
+            
+        }
+        else if (index == 1)
+        {
+            i = 1;
+            SecondColor();
+        }
+        else if (index == 2)
+        {
+            i = 2;
+            ThirdColor();
+        }
+    }
+
+    void FirstColor() {
+        cars[0].GetComponent<Renderer>().material = m1;
+        Debug.Log(cars[0].GetComponent<Renderer>().material);
+        Debug.Log(cars[0].GetComponent<Renderer>().materials);
+        Debug.Log(cars[0].GetComponent<Renderer>().materials[0]);
+    }
+
+    void SecondColor()
+    {
+        cars[0].GetComponent<Renderer>().material = m2;
+        Debug.Log(cars[0].GetComponent<Renderer>().material);
+        Debug.Log(cars[0].GetComponent<Renderer>().materials);
+        Debug.Log(cars[0].GetComponent<Renderer>().materials[0]);
+    }
+
+    void ThirdColor()
+    {
+        cars[0].GetComponent<Renderer>().material = m3;
+        Debug.Log(cars[0].GetComponent<Renderer>().material);
+        Debug.Log(cars[0].GetComponent<Renderer>().materials);
+        Debug.Log(cars[0].GetComponent<Renderer>().materials[0]);
+    }
+}
